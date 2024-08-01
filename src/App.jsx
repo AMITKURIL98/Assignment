066f7aa032f4af4f7d9088fbc333bbf109e33603
AmitOpticalArc <amitkuril@opticalarc.com>
2024-08-01 10:00:04 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+
+
+import './App.css'
+import EmployeeTable from './Compoment/EmployeeTable'
+
+function App() {
+ 
+
+  return (
+    <>
+   <EmployeeTable/>
+    </>
+  )
+}
+
+export default App
